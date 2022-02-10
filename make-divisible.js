9585a818ae2ed6131679c6e6486aa9d1070effbf
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-02-10 13:47:04 -0600

    update

---
+++ make-divisible.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')();
+
+//Asks user to input a number
+let x = (Number(prompt("Enter first number: ")));
+
+//Asks user to input a second number
+let y = (Number(prompt("Enter second number: ")));
+
+
+//As long as 'x / y' does not equal a whole number, the loop will repeat
+while (x/y !== Math.round(x/y)) {
+
+//Loop will continue to add 1 to 'x' with each pass until it can be divided by 'y' with no remainder.    
+    x = x + 1;
+
+//Each pass will log out the the value of 'x + 1' until it reaches the value allowing it to be 
+//  divided by 'y'    
+    console.log(x);
+
+}
+
+//Message output to screen telling user that 'x' is divisible by 'y'
+console.log( x + " is divisible by " + y );
+
+//Ex.
+//  User inputs x:9, y:5
+//      Since 9 is not divisible by 5, on the first pass it will add 1 to 9(x).
+//          This now makes x = 10 which is divisible by 5, ending the loop and logging the output message.
--- string-repeater.js
+++ string-repeater.js
@@ -3,14 +3,19 @@
 
 const prompt = require('prompt-sync')();
 
+//Asks user for initial string input. 
 let input = prompt("Enter a string: ")
+
+//Declares a variable for the new string to be created when adding initial input with each pass of the loop.
 let combined = "";
+
+//Sets a starting point for the number of characters to be counted in the string.
 let initialLength = 0;
 
 
     //As long as the string length is less than 10, the while statement will repeat.
 while (initialLength <= 10){
-    
+
     //creating a new string by adding user input
     combined += input
 
@@ -21,3 +26,8 @@ while (initialLength <= 10){
     initialLength += input.length;
 
 } 
+
+//Ex.
+//  User inputs abc. abc is equal to '3' characters long.
+//      The string will now add abc to itself creating abcabc which is 6 characterslong.
+//          This process will continue until the number of characters in the string is greater than or equal to 10.
