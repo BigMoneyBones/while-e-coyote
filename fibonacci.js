63e60d11531d5d2b41875716e5294425f608e82a
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-02-10 16:56:03 -0600

    update sude code

--- fibonacci.js
+++ fibonacci.js
@@ -32,9 +32,9 @@ if (n > 0) {
 //  A broken down example looks like this: 
 //      User input is 5.
 //      Pass 1: console logs 'a' or 0 to the screen. 0 is given +1 at the a++ statement, the else statement is ignored because count is equal to 0. count is given +1 at the count ++ statement.
-//      Pass 2: console logs 'a' or 1 to the screen. The count is no longer equal to 0 so the else statement reads 1 += 1 assigning 'a' the new value of 2. if 'b' = 'a'(2) - 'b'(1) then 'b' becomes 1.
-//      Pass 3: console logs 'a' or 2 to the screen. The count is no longer equal to 0 so the else statement reads 2 += 1 assigning 'a' the new value of 3. if 'b' = 'a'(3) - 'b'(1) then 'b' becomes 2.
-//      Pass 4: console logs 'a' or 3 to the screen. The count is no longer equal to 0 so the else statement reads 3 += 2 assigning 'a' the new value of 5. if 'b' = 'a'(5) - 'b'(2) then 'b' becomes 3.
-//      Pass 5: console logs 'a' or 5 to the screen. The count is no longer equal to 0 so the else statement reads 5 += 3 assigning 'a' the new value of 8. if 'b' = 'a'(8) - 'b'(3) then 'b' becomes 5.
+//      Pass 2: console logs 'a' or 1 to the screen. The count is no longer equal to 0 so the else statement reads 1 += 1 assigning 'a' the new value of 2.  if 'b' = 'a'(2)  - 'b'(1) then 'b' becomes 1.
+//      Pass 3: console logs 'a' or 2 to the screen. The count is no longer equal to 0 so the else statement reads 2 += 1 assigning 'a' the new value of 3.  if 'b' = 'a'(3)  - 'b'(1) then 'b' becomes 2.
+//      Pass 4: console logs 'a' or 3 to the screen. The count is no longer equal to 0 so the else statement reads 3 += 2 assigning 'a' the new value of 5.  if 'b' = 'a'(5)  - 'b'(2) then 'b' becomes 3.
+//      Pass 5: console logs 'a' or 5 to the screen. The count is no longer equal to 0 so the else statement reads 5 += 3 assigning 'a' the new value of 8.  if 'b' = 'a'(8)  - 'b'(3) then 'b' becomes 5.
 //      Pass 6: console logs 'a' or 8 to the screen. The count is no longer equal to 0 so the else statement reads 8 += 5 assigning 'a' the new value of 13. if 'b' = 'a'(13) - 'b'(5) then 'b' becomes 8.
 //      etc...
